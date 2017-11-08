8f9e170614e7cb486c8102a77c387d2b23f6d5d4
Benardi <benardinunes@gmail.com>
2017-11-08 00:35:53 -0300

    Small function to map content of cell to its due output

--- imperative_project/marel_imperative.cpp
+++ imperative_project/marel_imperative.cpp
@@ -1,19 +1,32 @@
 #include <iostream>
 using namespace std;
 
-int marel_board [3][3] = {{ 0,1,2 }, {3,4,5}, {6,7,8} };
+int marel_board [3][3] = { {0,1,2}, {1, 0,2}, {1,2,0} };
 
 
-void snapshot_board(int platform [3][3]) {
 
+char map_cell(int content){
+  if(content == 1){
+    return 'X';
+  }
+  else if (content == 2){
+    return 'O';
+  }
+  else {
+   return '_';
+  }
+
+}
+
+void snapshot_board(int platform [3][3]){
+  endl(cout);
   for(int i = 0; i < 3; i++){
     for(int j = 0; j < 3; j++){
-      cout << platform[i][j] << ' ';
+      cout << map_cell(platform[i][j]) << ' ';
     }
     endl(cout);
   }
-
-
+  endl(cout);
 }
 
 int main()
