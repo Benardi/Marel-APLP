5e02747da00b4d47d28513eecf4794d88b6b91ce
Benardi <benardinunes@gmail.com>
2017-11-26 09:57:27 -0300

    Implementation of function to place pieces

--- functional_project/marel.hs
+++ functional_project/marel.hs
@@ -48,9 +48,23 @@ check_victory shape board = do
     then True
   else False
 
+create_board :: [Char] -> [Char] -> [Char]-> [[Char]]
+create_board row1 row2 row3 = [row1,row2, row3]
+
+
+slice from to xs = take (to - from + 1) (drop from xs)
+
+addVal :: [Char] -> [Char] -> [Char]-> [[Char]]
+addVal row1 row2 row3 = [row1,row2, row3]
+
+place_piece :: Char -> Int -> Int -> [[Char]] -> [[Char]]
+place_piece shape row column board = do
+    let flat = [if x == row && y == column then shape else ((board !! x) !! y) | x <- [0..2], y <- [0..2]]
+    addVal (slice 0 2 flat) (slice 3 5 flat) (slice 6 8 flat)
 
 main :: IO ()
 main = do
     shape <- getChar
     let marel_board  = [['X','X','_'],['_','X','_'],['_','_','_']]
     print (check_victory shape marel_board)
+    print (place_piece 'X' 2 1 marel_board)
